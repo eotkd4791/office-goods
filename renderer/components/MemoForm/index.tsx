882290e29316ff9998665d32b9e3229938dec85c
eotkd4791 <eotkd4791@gmail.com>
2022-07-18 00:46:05 +0900

    ✨ Feat: 메모 작성 폼 역할을 하는 컴포넌트 작성

---
+++ renderer/components/MemoForm/index.tsx
@@ -0,0 +1,60 @@
+import { FC, MouseEventHandler } from 'react';
+import { useForm } from 'react-hook-form';
+
+import useStore from 'renderer/store';
+
+interface Props {
+  onClick: MouseEventHandler<HTMLButtonElement>;
+  toggleMemo: () => void;
+}
+
+const MemoForm: FC<Props> = ({ onClick, toggleMemo }) => {
+  const closeAll = useStore((state) => state.closeAll);
+
+  const { handleSubmit } = useForm();
+
+  const closeMemo = () => {
+    closeAll();
+  };
+
+  const onSubmit = handleSubmit(() => {
+    toggleMemo();
+  });
+
+  return (
+    <div className="flex flex-col w-[25rem]">
+      <aside className="fixed w-[100vw] h-[100vh] top-0 left-0" onClick={closeMemo} />
+      <form onSubmit={onSubmit} className="relative z-10 flex flex-col form-control">
+        <header>
+          <label
+            htmlFor="memo"
+            className="flex items-center justify-between py-2 rounded-t-lg bg-primary text-primary-content focus:w-[404px]"
+          >
+            <h3 className="pl-4 text-xl select-none">Memo</h3>
+            <button className="flex items-center justify-center btn btn-ghost" onClick={onClick}>
+              __
+            </button>
+          </label>
+        </header>
+        <textarea
+          name="memo"
+          id="memo"
+          placeholder="메모를 입력해주세요."
+          className="w-full rounded-t-none resize-none textarea textarea-primary"
+          cols={30}
+          rows={10}
+        />
+        <footer className="self-end w-[10rem] mt-4 flex justify-between">
+          <button type="submit" className="w-[48%] btn btn-info">
+            저장
+          </button>
+          <button onClick={closeMemo} className="w-[48%] btn btn-ghost btn-outline">
+            취소
+          </button>
+        </footer>
+      </form>
+    </div>
+  );
+};
+
+export default MemoForm;
