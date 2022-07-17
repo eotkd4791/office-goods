9722c3f044d087f55a2e15b599617ed62b8c6cc1
eotkd4791 <eotkd4791@gmail.com>
2022-07-18 00:45:39 +0900

    ✨ Feat: 빠른 메모 작성을 위한 버튼 컴포넌트

---
+++ renderer/components/ButtonAddMemo/index.tsx
@@ -0,0 +1,37 @@
+import { FC, MouseEventHandler } from 'react';
+import { BsPencilSquare as Pencil } from 'react-icons/bs';
+import useStore from 'renderer/store';
+import MemoForm from 'renderer/components/MemoForm';
+
+const ButtonAddMemo: FC = () => {
+  const { visibleMemo, toggleMemo } = useStore((state) => ({
+    visibleMemo: state.visible.memo,
+    toggleMemo: state.toggleMemo,
+  }));
+
+  const onClick: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+    toggleMemo();
+  };
+
+  return (
+    <>
+      <aside className="fixed bottom-8 right-8">
+        {visibleMemo ? (
+          <MemoForm onClick={onClick} toggleMemo={toggleMemo} />
+        ) : (
+          <div className="tooltip hover:tooltip-open" data-tip="ctrl + n">
+            <button
+              className="flex items-center justify-center w-16 h-16 btn-primary text-primary-content btn-circle"
+              onClick={onClick}
+            >
+              <Pencil className="w-7 h-7" />
+            </button>
+          </div>
+        )}
+      </aside>
+    </>
+  );
+};
+
+export default ButtonAddMemo;
